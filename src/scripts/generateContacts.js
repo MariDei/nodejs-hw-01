c2b090c9cb6a2852df00302db2f00b243e1f2ab1
Marianna Dei <d.marianna@icloud.com>
2024-07-21 18:00:53 +0300

    Added code to function generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,27 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (numContacts) => {
+  try {
+    let contacts = [];
+    const data = await fs.readFile(PATH_DB, { encoding: 'UTF-8' });
+    contacts = JSON.parse(data);
+
+    const newContacts = [];
+    for (let i = 0; i < numContacts; i += 1) {
+      newContacts.push(createFakeContact());
+    }
+
+    const updContacts = contacts.concat(newContacts);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(updContacts, null, 2));
+    console.log(
+      `Successfully generated and added ${numContacts} contacts to db.json`,
+    );
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 generateContacts(5);

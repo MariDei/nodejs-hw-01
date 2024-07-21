1d8fde651a6130866b8a96e3c878448a0a33a63f
Marianna Dei <d.marianna@icloud.com>
2024-07-21 18:10:10 +0300

    Added code to function removeAllContacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([], null, 2));
+    console.log('All contacts removed successfully.');
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+  }
+};
 
 removeAllContacts();

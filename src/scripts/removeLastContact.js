ceb2148038a33bf5e01bc55895cdcf6895890c91
Marianna Dei <d.marianna@icloud.com>
2024-07-21 18:12:21 +0300

    Added code to function removeLastContacts

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,21 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const contacts = JSON.parse(
+      await fs.readFile(PATH_DB, { encoding: 'UTF-8' }),
+    );
+    if (contacts.length > 0) {
+      contacts.pop();
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+      console.log('Last contact removed successfully.');
+    } else {
+      console.log('No contacts to remove.');
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+};
 
 removeLastContact();

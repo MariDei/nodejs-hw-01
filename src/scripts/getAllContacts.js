4c3f039dbf035a4f96eb2d8d4235047033a7147d
Marianna Dei <d.marianna@icloud.com>
2024-07-21 18:06:29 +0300

    Added code to function getAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,15 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, { encoding: 'UTF-8' });
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error('Error reading data from file:', error);
+    return [];
+  }
+};
 
 console.log(await getAllContacts());
